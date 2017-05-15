21f53da86f30303d7f1215f9ac238bfc8ced3466
Craig Franklin <cfranklin@marketplacer.com>
2017-05-15 23:37:19 +1000

    Add programs list & component

--- app/javascript/budgethack/components/programs.jsx
+++ app/javascript/budgethack/components/programs.jsx
@@ -2,18 +2,39 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
 import List from './list';
+import Program from './program';
 
 export default class Programs extends Component {
   static propTypes = {
     departments: PropTypes.object,
+    addProgram: PropTypes.func,
+    addedPrograms: PropTypes.arrayOf(PropTypes.object),
   }
 
   static defaultProps = {
     departments: {},
+    addedPrograms: [],
+  }
+
+  constructor () {
+    super();
+
+    this.state = {
+      isListVisible: false,
+    };
+  }
+
+  showPrograms = () => {
+    this.setState({ isListVisible: true });
+  }
+
+  addProgram = (name) => {
+    this.props.addProgram(name);
+    this.setState({ isListVisible: false });
   }
 
   render () {
-    const { data, currentDepartment } = this.props.departments;
+    const { departments: { data, currentDepartment, addedPrograms } } = this.props;
     const department = data && data.filter((dept) => { return dept.name === currentDepartment; });
     const { name, programs } = department ? department[0] : {};
     const budgets = programs ? programs.map((program) => { return program.budgets; }) : [];
@@ -47,35 +68,50 @@ export default class Programs extends Component {
       <div className="programs">
         <h1 className="programs__department-name">
           <img className="programs__icon" src="http://placehold.it/36x36" />
-         { name }
-       </h1>
-        { chartData && (
-          <BarChart width={750} height={250} data={chartData}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="value" fill="#8884d8" />
-          </BarChart>
-        ) }
-        <div>
-          <ul>
-            <li>{`2017/18 Budget: $${currentBudget}`}</li>
-            <li>{`Change from Prev. Year: ${change}%`}</li>
-          </ul>
-        </div>
-        <h1>
-          <Link to="/">Programs</Link>
-
+          { name }
         </h1>
-        <h2 className="list__title">
-          Select a Program:
-        </h2>
+        { !this.state.isListVisible && (
+          <div>
+            { chartData && (
+              <BarChart width={750} height={250} data={chartData}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="value" fill="#8884d8" />
+              </BarChart>
+            ) }
+            <div>
+              <ul>
+                <li>{`2017/18 Budget: $${currentBudget}`}</li>
+                <li>{`Change from Prev. Year: ${change}%`}</li>
+              </ul>
+            </div>
+            <h1>
+              <Link to="/">Programs</Link>
+
+            </h1>
+            <ul>
+              { addedPrograms.map((program, index) => {
+                return (<Program key={index} name={program.name} />);
+              })
+              }
+            </ul>
+            <h2 className="list__title">
+              Select a Program:
+            </h2>
+            <button type="button" onClick={this.showPrograms}>Add a Program</button>
+          </div>
+        ) }
         <div className="photo-grid">
-
-          <List  items = { programs } isPrograms />
-
+          { this.state.isListVisible && (
+            <List
+              items={programs}
+              isPrograms
+              addProgram={this.addProgram}
+            />
+          ) }
         </div>
       </div>
     );
