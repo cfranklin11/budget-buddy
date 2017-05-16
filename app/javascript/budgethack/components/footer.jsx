361d21c1bc573e28f25571a0991e4263addc29d2
melissa gattoni <meligatt@gmail.com>
2017-05-17 00:21:30 +1000

    add footer and nav styles

--- app/javascript/budgethack/components/footer.jsx
+++ app/javascript/budgethack/components/footer.jsx
@@ -7,13 +7,16 @@ class Footer extends React.Component {
 
   render() {
     return (
-        <footer className="footer">
-          <span className="footer__logo">BUDGETHACK 2017</span>
-          <br />
-          <span  className="footer__info">Made with lots of ☕☕☕  by <a href="https://github.com/cfranklin11/budget-hack-2017">The Hippo’s stripes</a></span>
-          <br />
-          <span  className="footer__social">twitter | facebook | google +</span>
-        </footer>
+      <footer className="footer">
+        <span className="footer__logo">BUDGETHACK 2017</span>
+        <br />
+        <span  className="footer__info">Made with lots of ☕☕☕  by <a href="https://github.com/cfranklin11/budget-hack-2017">The Hippo’s stripes</a></span>
+        <ul className="footer-nav">
+          <li><a href="https://github.com/meligatt/budgethack2017_frontend">About</a></li>
+          <li><a href="https://github.com/meligatt/budgethack2017_frontend">Github</a></li>
+          <li><a href="https://github.com/meligatt/budgethack2017_frontend">Contact</a></li>
+        </ul>
+      </footer>
     );
   }
 }
