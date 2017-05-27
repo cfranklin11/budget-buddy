6397f5f56a81c20d9476732856ca63a160e2214e
Craig Franklin <craigjfranklin@gmail.com>
2017-05-27 17:03:08 +1000

    Add stub to department test for consistency

--- spec/controllers/data_controller_spec.rb
+++ spec/controllers/data_controller_spec.rb
@@ -13,7 +13,14 @@ describe DataController do
 
   describe 'GET department' do
     it 'returns json' do
-      get :department, params: { department_name: 'Department of Health and Human Services' }
+      allow(controller).to receive(:department_data).and_return(
+        name: 'Department Name',
+        current_budget: 2000,
+        prev_budget: 1500,
+        programs: []
+      )
+
+      get :department, params: { department_name: 'Department Name' }
       expect(response.code).to eq('200')
       expect(response.content_type).to eq('application/json')
     end
